ad99d2c16b7a3472666b9620f94a71806f1db374
João <joaoaugusto311016@gmail.com>
2023-05-30 14:16:04 -0300

    30-05

--- src/br/com/alura/screematch/calculos/CalculadoraDeTempo.java
+++ src/br/com/alura/screematch/calculos/CalculadoraDeTempo.java
@@ -1,7 +1,4 @@
 package br.com.alura.screematch.calculos;
-
-import br.com.alura.screematch.modelos.Filme;
-import br.com.alura.screematch.modelos.Serie;
 import br.com.alura.screematch.modelos.Titulo;
 
 public class CalculadoraDeTempo {
--- src/br/com/alura/screematch/modelos/Episodio.java
+++ src/br/com/alura/screematch/modelos/Episodio.java
@@ -1,9 +1,20 @@
 package br.com.alura.screematch.modelos;
 
-public class Episodio {
+import br.com.alura.screematch.calculos.Classificavel;
+
+public class Episodio implements Classificavel {
     private int numero;
     private String nome;
     private Serie serie;
+    private int totalVizu;
+
+    public int getTotalVizu() {
+        return totalVizu;
+    }
+
+    public void setTotalVizu(int totalVizu) {
+        this.totalVizu = totalVizu;
+    }
 
     public int getNumero() {
         return numero;
@@ -27,5 +38,14 @@ public class Episodio {
 
     public void setSerie(Serie serie) {
         this.serie = serie;
+    }
+
+    @Override
+    public int getClassificacao() {
+        if(totalVizu >= 100){
+            return 4;
+        } else {
+            return 2;
+        }
     }
 }
