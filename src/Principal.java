67beefb88eb98dbea5ecd73b30c74d0b88b29900
João <joaoaugusto311016@gmail.com>
2023-06-09 11:26:24 -0300

    09-06

--- src/Principal.java
+++ src/Principal.java
@@ -1,69 +1,54 @@
-import br.com.alura.screematch.calculos.CalculadoraDeTempo;
-import br.com.alura.screematch.calculos.Recomendacoes;
-import br.com.alura.screematch.modelos.Episodio;
-import br.com.alura.screematch.modelos.Filme;
-import br.com.alura.screematch.modelos.Serie;
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.setNome("Poderoso chefão");
-        meuFilme.setAnoDeLancamento(2002);
-        meuFilme.setDuracaoEmMinutos(170);
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
-        meuFilme.exibeFichaTecninca();
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
         meuFilme.avalia(10);
-        meuFilme.avalia(7.0);
-        meuFilme.avalia(6);
-
-        System.out.println(meuFilme.getTotalDeAvaliacoes());
-        System.out.println("Soma das Avaliações:" + meuFilme.mediaDasAvaliacoes());
-        System.out.println("**************************************************");
-        System.out.println();
-        System.out.println("Nome: " + meuFilme.getNome());
-        System.out.println("Ano de lançamento: " + meuFilme.getAnoDeLancamento());
-        System.out.println("Duração em minutos: " + meuFilme.getDuracaoEmMinutos());
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+        //meuFilme.somaDasAvaliacoes = 10;
+        //meuFilme.totalDeAvaliacoes = 1;
+        //System.out.println(meuFilme.pegaMedia());
 
         Serie lost = new Serie();
         lost.setNome("Lost");
-        lost.setAnoDeLancamento(2002);
-        lost.setDuracaoEmMinutos(200);
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
         lost.setTemporadas(10);
-        lost.setEpisodiosPorTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
-
-        System.out.println("*************************");
-        System.out.println("Nome da Serie: " + lost.getNome());
-        System.out.println("Ano de Lançamento: " + lost.getAnoDeLancamento());
         System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
 
-        Filme naruto = new Filme();
-        naruto.setNome("Avatar");
-        naruto.setAnoDeLancamento(2023);
-        naruto.setDuracaoEmMinutos(250);
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
 
-
-        System.out.println("******************************");
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
-
         calculadora.inclui(meuFilme);
-        calculadora.inclui(naruto);
+        calculadora.inclui(outroFilme);
         calculadora.inclui(lost);
-        System.out.println("Tempo que ficarei assistindo: " + calculadora.getTempoTotal());
+        System.out.println(calculadora.getTempoTotal());
 
-        System.out.println("**********************");
-        System.out.println("Recomendações");
-        Recomendacoes filtro = new Recomendacoes();
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
         filtro.filtra(meuFilme);
 
-        System.out.println("**********************");
-        System.out.println("Episodio");
         Episodio episodio = new Episodio();
         episodio.setNumero(1);
         episodio.setSerie(lost);
-        episodio.setTotalVizu(300);
+        episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
-
     }
-
 }
--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,4 +1,4 @@
-package br.com.alura.screematch.modelos;
+package br.com.alura.screenmatch.modelos;
 
 public class Titulo {
     private String nome;
